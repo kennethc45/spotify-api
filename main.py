47384b4e0b66aeaf5f6a87cfec81804d01ab2484
kennethc45 <kennethcadungog45@gmail.com>
2024-10-23 10:04:36 -0500

    Able to search for artist in Spotify

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import json
 from dotenv import load_dotenv
 import os
 import base64
-from requests import post 
+from requests import post, get
 
 load_dotenv()
 
 client_id:str = os.getenv("CLIENT_ID")
 client_secret:str = os.getenv("CLIENT_SECRET")
 
-print(f"Client ID: {client_id}")
-print(f"Client Secret: {client_secret}")
+# print(f"Client ID: {client_id}")
+# print(f"Client Secret: {client_secret}")
 
 def get_token():
     auth_string:str = client_id + ":" + client_secret
@@ -30,5 +30,26 @@ def get_token():
 
     return token 
 
+def get_auth_headers(token):
+    return {"Authorization": "Bearer " + token}
+
+def search_for_artist(token, artist_name):
+    url:str = "https://api.spotify.com/v1/search"
+    headers = get_auth_headers(token)
+    query:str = f"q={artist_name}&type=artist&limit=1"
+
+    query_url:str = url + "?" + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["artists"]["items"]
+    if len(json_result) == 0:
+        print("No artist with this name exists...")
+        return None
+    
+    return json_result[0]
+
+
+
 token = get_token()
-print(f"Token: {token}")
+# print(f"Token: {token}")
+result = search_for_artist(token, "ACDC")
+print(f"Result: {result["name"]}")
